5504503b1b8d5656e02f6754d58caebafebfa0d0
smerch88 <maxarmyk@gmail.com>
2023-05-16 18:17:42 +0300

    feat: add react query

--- components/SearchWidget.tsx
+++ components/SearchWidget.tsx
@@ -2,36 +2,32 @@ import { Section } from '@/components/Section';
 import { Title } from '@/components/Title';
 import { UserCardMini } from '@/components/UserCardMini';
 import { User } from '@/types';
-import { FC, useEffect, useState } from 'react';
+import { FC, useState } from 'react';
+import { useQuery } from 'react-query';
 
 export const SearchWidget: FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
-  const [suggestedUsers, setSuggestedUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    const fetchSuggestedUsers = async () => {
-      if (searchTerm) {
-        try {
-          const res = await fetch(
-            `https://dummyjson.com/users/search?q=${searchTerm}`,
-          );
-          const data = await res.json();
+  const {
+    data: suggestedUsers = [],
+    isLoading,
+    isError,
+  } = useQuery<User[]>(['suggestedUsers', searchTerm], async () => {
+    if (searchTerm) {
+      const res = await fetch(
+        `https://dummyjson.com/users/search?q=${searchTerm}`,
+      );
+      const data = await res.json();
 
-          if (!res.ok) {
-            throw new Error(data.message);
-          }
-
-          setSuggestedUsers(data.users);
-        } catch (error) {
-          console.error('Error fetching suggested users:', error);
-          setSuggestedUsers([]);
-        }
-      } else {
-        setSuggestedUsers([]);
+      if (!res.ok) {
+        throw new Error(data.message);
       }
-    };
-    fetchSuggestedUsers();
-  }, [searchTerm]);
+
+      return data.users;
+    } else {
+      return [];
+    }
+  });
 
   return (
     <Section className="bg-primary">
@@ -50,20 +46,26 @@ export const SearchWidget: FC = () => {
           placeholder="Name.."
         />
         <div className="relative">
-          <ul className="absolute left-0 top-0 z-10 max-h-[400px] w-full max-w-[320px] gap-2 overflow-y-scroll rounded-s bg-dark">
-            {suggestedUsers.map((user) => (
-              <li key={user.id}>
-                <UserCardMini
-                  id={user.id}
-                  firstName={user.firstName}
-                  lastName={user.lastName}
-                  image={user.image}
-                  variant="light"
-                  direction="horizontal"
-                />
-              </li>
-            ))}
-          </ul>
+          {isLoading ? (
+            <div>Loading...</div>
+          ) : isError ? (
+            <div>Error fetching suggested users.</div>
+          ) : (
+            <ul className="absolute left-0 top-0 z-10 max-h-[400px] w-full max-w-[320px] gap-2 overflow-y-scroll rounded-s bg-dark">
+              {suggestedUsers.map((user) => (
+                <li key={user.id}>
+                  <UserCardMini
+                    id={user.id}
+                    firstName={user.firstName}
+                    lastName={user.lastName}
+                    image={user.image}
+                    variant="light"
+                    direction="horizontal"
+                  />
+                </li>
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </Section>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,17 @@
 import type { AppProps } from 'next/app';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import '@/styles/globals.css';
 import 'swiper/css';
 
+const queryClient = new QueryClient();
+
 const App = ({ Component, pageProps = {} }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
