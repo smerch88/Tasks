0f2031efcd6f7e1794ce4f974674e163ac8872ef
smerch88 <maxarmyk@gmail.com>
2023-05-17 21:28:14 +0300

    feat: add debounce, cache, racing

--- components/SearchWidget.tsx
+++ components/SearchWidget.tsx
@@ -1,36 +1,61 @@
 import { Section } from '@/components/Section';
 import { Title } from '@/components/Title';
 import { UserCardMini } from '@/components/UserCardMini';
+import { useDebounceValue } from '@/hooks/useDebounceValue';
 import { User } from '@/types';
 import { FC, useEffect, useState } from 'react';
 
 export const SearchWidget: FC = () => {
   const [searchTerm, setSearchTerm] = useState<string>('');
   const [suggestedUsers, setSuggestedUsers] = useState<User[]>([]);
+  const [cache, setCache] = useState<Record<string, User[]>>({});
+  const [fetchController, setFetchController] =
+    useState<AbortController | null>(null);
+
+  const debouncedSearchTerm = useDebounceValue(searchTerm, 300);
 
   useEffect(() => {
     const fetchSuggestedUsers = async () => {
-      if (searchTerm) {
-        try {
-          const res = await fetch(
-            `https://dummyjson.com/users/search?q=${searchTerm}`,
-          );
-          const data = await res.json();
+      if (debouncedSearchTerm) {
+        if (cache[debouncedSearchTerm]) {
+          setSuggestedUsers(cache[debouncedSearchTerm]);
+        } else {
+          const controller = new AbortController();
+          setFetchController(controller);
 
-          if (!res.ok) {
-            throw new Error(data.message);
+          try {
+            const res = await fetch(
+              `https://dummyjson.com/users/search?q=${debouncedSearchTerm}`,
+              { signal: controller.signal },
+            );
+            const data = await res.json();
+
+            if (!res.ok) {
+              throw new Error(data.message);
+            }
+
+            const users = data.users || [];
+            setCache((prevCache) => ({
+              ...prevCache,
+              [debouncedSearchTerm]: users,
+            }));
+            setSuggestedUsers(users);
+          } catch (error) {
+            setSuggestedUsers([]);
           }
-
-          setSuggestedUsers(data.users);
-        } catch (error) {
-          setSuggestedUsers([]);
         }
       } else {
         setSuggestedUsers([]);
       }
     };
+
+    if (fetchController) {
+      fetchController.abort();
+    }
+
     fetchSuggestedUsers();
-  }, [searchTerm]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedSearchTerm, cache]);
 
   return (
     <Section className="bg-primary">
---
+++ hooks/useDebounceValue.ts
@@ -0,0 +1,17 @@
+import { useState, useEffect } from 'react';
+
+export const useDebounceValue = <T>(value: T, delay: number): T => {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
